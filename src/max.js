83f149a6f34673d6049818519cf40f480f007bb1
Sowmya Muduganti <si@Sowmyas-MBP.Dlink>
2021-01-14 16:00:00 +0530

    add min and max functions

---
+++ src/max.js
@@ -0,0 +1,4 @@
+const max = (arr) => {
+    return Math.max( ...arr);
+}
+module.exports = max;
---
+++ src/min.spec.js
@@ -0,0 +1,9 @@
+const min = require('./min');
+
+describe('Min', () => {
+
+    it('min([1,2,3,4]) is 1', () => {
+        expect(min([1,2,3,4])).toEqual(1);
+    });
+
+})
