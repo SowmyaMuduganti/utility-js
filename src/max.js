3eaae0973afb8d927e29dc9d9073be4cf60a41ea
Sowmya Muduganti <si@Sowmyas-MBP.Dlink>
2021-01-16 16:53:48 +0530

    change implementation of max function

--- src/max.js
+++ src/max.js
@@ -1,4 +1,10 @@
 const max = (arr) => {
-    return Math.max( ...arr);
+    var maxOfArr = 0;
+    for(i=0;i<arr.length;i++){
+        if(arr[i]>maxOfArr){
+            maxOfArr = arr[i];
+        }
+    }
+    return maxOfArr;
 }
 module.exports = max;
