2e381132d66a8547f28a2dc1c2fb1407e75dbf14
Sowmya Muduganti <si@Sowmyas-MBP.Dlink>
2021-01-16 16:56:09 +0530

    change implementation of min function

--- src/min.js
+++ src/min.js
@@ -1,4 +1,10 @@
 const min = (arr) => {
-    return Math.min( ...arr);
+    var minOfArr = arr[0];
+    for(i=1;i<arr.length;i++){
+        if(arr[i]<minOfArr){
+            minOfArr = arr[i];
+        }
+    }
+    return minOfArr;
 }
 module.exports = min;
